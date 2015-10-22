31d36fa84a4e60709a1b64d0967782a576934af7
Chris Sheppard <chris.sheppard@thebioagency.com>
2015-10-22 10:29:07 +0100

    Adding pixel art scaling

--- example/example.ts
+++ example/example.ts
@@ -1,8 +1,13 @@
+/// <reference path="../typings/pixi.js/pixi.js.d.ts" />
+
 import Game from "../src/engine-ts/game/Game";
 import Graphics from "../src/engine-ts/graphics/Graphics";
 import SceneManager from "../src/engine-ts/scene/SceneManager";
 
 import BootstrapScene from "./scene/BootstrapScene";
+
+// Pixel art scaling
+PIXI.SCALE_MODES.DEFAULT = PIXI.SCALE_MODES.NEAREST;
 
 // Initialise the graphics
 Graphics.init(document.body, 800, 600);
